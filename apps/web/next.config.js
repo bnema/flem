779d43b508d6b458af3627354012b1b94b4b18ed
Brice Amen <brice@briceamen.com>
2023-05-09 13:23:46 +0200

    Switch to yarn

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+/** @type {import('next').NextConfig} */
+
+const { dependencies } = require('./package.json')
+
+// init
+ const transpilePackages = Object
+ .keys(dependencies)
+ .filter((dependency) => dependency.includes('@ex-yarn-turborepo-demo/'))
+
+ // main
+ const nextConfig = {
+ reactStrictMode: true,
+ swcMinify: true,
+ transpilePackages
+ }
+
+// export
+ module.exports = nextConfig
---
+++ packages/types/index.ts
@@ -0,0 +1,11 @@
+// basic types in typescript
+
+// user type
+
+export type User = {
+    name: string;
+    age: number;
+    occupation: string;
+    isMarried: boolean;
+    hobbies: string[];
+}
