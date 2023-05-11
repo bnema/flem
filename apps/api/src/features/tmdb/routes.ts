78aeec4d06669960d9554b5ed20b7c541ae4a573
Brice Amen <brice@briceamen.com>
2023-05-11 11:15:30 +0200

    Fixed while loop for empty overviews in random10 route

--- apps/api/src/features/tmdb/routes.ts
+++ apps/api/src/features/tmdb/routes.ts
@@ -13,80 +13,80 @@ export const registerTmdbRoutes = (fastify: FastifyInstance) => {
     "/v1/tmdb/movies/post/title",
     async (request, reply) => {
       try {
-      const { titles } = request.body;
-      const results = await Promise.all(
-        titles.map(async (title) => {
-          const movies = await searchMoviesByTitle(title);
+        const { titles } = request.body;
+        const results = await Promise.all(
+          titles.map(async (title) => {
+            const movies = await searchMoviesByTitle(title);
 
-          return Promise.all(
-            movies.map(async (movie: Movie) => {
-              const details = await getMovieDetails(movie.id);
-              return details;
-            })
-          );
-        })
-      );
+            return Promise.all(
+              movies.map(async (movie: Movie) => {
+                const details = await getMovieDetails(movie.id);
+                return details;
+              })
+            );
+          })
+        );
 
-      reply.send(results.flat());
-    } catch (err) {
-      console.error(err);
-      reply.status(500).send({ error: "Something went wrong" });
+        reply.send(results.flat());
+      } catch (err) {
+        console.error(err);
+        reply.status(500).send({ error: "Something went wrong" });
+      }
     }
-  }
   );
 
-  fastify.post<{ Body: { ids: number[] } }>( 
-  // Route to return all the movies from a given list of IDs
-  "/v1/tmdb/movies/post/ids",
+  fastify.post<{ Body: { ids: number[] } }>(
+    // Route to return all the movies from a given list of IDs
+    "/v1/tmdb/movies/post/ids",
     async (request, reply) => {
       try {
-      const { ids } = request.body;
-      const results = await Promise.all(
-        ids.map(async (id) => {
-          const details = await getMovieDetails(id);
-          return details;
-        })
-      );
+        const { ids } = request.body;
+        const results = await Promise.all(
+          ids.map(async (id) => {
+            const details = await getMovieDetails(id);
+            return details;
+          })
+        );
 
+        reply.send(results);
+      } catch (err) {
+        console.error(err);
+        reply.status(500).send({ error: "Something went wrong" });
+      }
+    }
+  );
+
+  fastify.get("/v1/tmdb/random10", async (request, reply) => {
+    try {
+      // Call the 'getMinMaxMovieID' function to get the minimum and maximum movie IDs
+      const { minID, maxID } = await getMinMaxMovieID();
+
+      // Initialize an empty results array
+      const results = [];
+
+      // Keep fetching a movie if the overview is empty or until the results length is 10
+      while (results.length < 10) {
+        const id = Math.floor(Math.random() * (maxID - minID + 1) + minID);
+        const details = await getMovieDetails(id);
+
+        // If the overview is not empty, push the details to the results array
+        if (
+          details.overview &&
+          details.overview !== "" &&
+          details.overview !== null
+        ) {
+          results.push(details);
+        } else {
+          console.log(`Empty overview for movie ${id}, refetching...`);
+        }
+      }
+
+      // Send the movie details as a JSON response
       reply.send(results);
     } catch (err) {
+      // If there is an error, log it to the console and send a 500 error response
       console.error(err);
       reply.status(500).send({ error: "Something went wrong" });
     }
-  }
-  );
-
-
-fastify.get('/v1/tmdb/random10', async (request, reply) => {
-  try {
-    // Call the 'getMinMaxMovieID' function to get the minimum and maximum movie IDs
-    const { minID, maxID } = await getMinMaxMovieID();
-
-    // Generate an array of 10 random movie IDs between the minimum and maximum values
-    const randomIds = Array.from({ length: 10 }, () => Math.floor(Math.random() * (maxID - minID + 1) + minID));
-
-    // Use Promise.all to fetch the details of all 10 movies
-    const results = await Promise.all(
-      randomIds.map(async (id) => {
-        let details = await getMovieDetails(id);
-        
-        // Keep fetching a new movie while the overview is empty
-        while (details.overview === "") {
-          console.log(`Empty overview for movie ${id}, refetching...`);
-          const newId = Math.floor(Math.random() * (maxID - minID + 1) + minID);
-          details = await getMovieDetails(newId);
-        }
-
-
-        return details;
-      }),
-    );
-    // Send the movie details as a JSON response
-    reply.send(results);
-  } catch (err) {
-    // If there is an error, log it to the console and send a 500 error response
-    console.error(err);
-    reply.status(500).send({ error: 'Something went wrong' });
-  }
-});
+  });
 };
