9dc906a76573b64066cab7f1b5a33a52e4481392
Brice Amen <brice@briceamen.com>
2023-05-24 13:12:03 +0200

    Function to get suggested movies from GPT3 and pass it to route : ok

--- apps/api/src/routes/ai.ts
+++ apps/api/src/routes/ai.ts
@@ -1,57 +1,46 @@
 import { FastifyInstance } from "fastify";
-import { Movie } from "@flem/types";
-import { searchMoviesByTitle, getMovieDetails } from "../features/tmdb/requests";
+import { Movie, SummaryItemMovie } from "@flem/types";
+import {
+  searchMoviesByTitle,
+  getMovieDetails,
+} from "../features/tmdb/requests";
 import { getMoviesFromGPT3 } from "../features/openai/handlers";
+
+export const createSummaryFromMoviesAndSendItToGPT3 = async (movies: Movie[]) => {
+  const summariesForGPT3: SummaryItemMovie[] = movies.map((movie) => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      release_date: movie.release_date,
+      genres: movie.genres,
+    };
+  });
+
+
+  const suggestedMoviesResponseFromGPT3 = await getMoviesFromGPT3(summariesForGPT3);
+  
+  console.log(suggestedMoviesResponseFromGPT3);
+  return suggestedMoviesResponseFromGPT3;
+};
+
 
 export const registerAIRoutes = (fastify: FastifyInstance) => {
   fastify.post<{ Body: { ids: number[] } }>(
     "/v1/ai/movies/post/ids",
     async (request, reply) => {
-        try {
-            // 1 - The user submit a list of movie IDs
-            const ids = request.body.ids; 
+      try {
+        const ids = request.body.ids;
+        const movies: Movie[] = (
+          await Promise.all(ids.map((id) => getMovieDetails(id, "english")))
+        ).filter((movie) => movie !== undefined);
 
-            // 2 - We retrieve the movies from the database
-            const movies: Movie[] = (await Promise.all(ids.map(id => getMovieDetails(id, 'english')))).filter(movie => movie !== undefined);;
-            // 3 - We create a small summary of each movie
-            const summaries = movies.map(movie => {
-                return `${movie.title} - ${new Date(movie.release_date).getFullYear()} (${movie.genres.map(genre => genre.name).join(', ')})`;
-            });
+        const detailedMovies = await createSummaryFromMoviesAndSendItToGPT3(movies);
+        reply.send(detailedMovies);
 
-            // 4 - We add all the summaries into the chatgpt's prompt and we ask to retrieve 20 more movies based on those added summaries
-            const prompt = {
-              'role': 'system',
-              'content': `Based on these summaries, please suggest 20 more movies and returns it as a formatted JSON: ${summaries.join('\n')}`,
-            };
-
-            // 5 - We retrieve the movies from GPT-3 response
-            const suggestedMovies = await getMoviesFromGPT3(prompt);
-            const suggestedMoviesString = suggestedMovies.join("");
-
-
-            // 6 Catch the beginning and the end "{...}" of the JSON inside the GPT-3 response and create a JSON object
-            const suggestedMoviesCreateJSON = suggestedMoviesString
-  .split("{")
-  .filter((item) => item.includes("}"))
-  .map((item) => `{${item}`);
-
-
-  const suggestedMoviesJSON = JSON.parse(suggestedMoviesCreateJSON[0]);
-            // 7 - We search by the title of each movie to retrieve more details
-            const detailedMovies = await Promise.all(
-              suggestedMoviesJSON.map(async (movie) => {
-                const movies = await searchMoviesByTitle(movie);
-                return movies[0];
-              })
-            );
-
-
-
-            reply.send(detailedMovies);
-        } catch (err) {
-            console.error(err);
-            reply.status(500).send({ error: "Something went wrong" });
-        }
-        }
+      } catch (err) {
+        console.error(err);
+        reply.status(500).send({ error: "Something went wrong" });
+      }
+    }
   );
 };
